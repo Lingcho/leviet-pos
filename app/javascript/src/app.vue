ca40e0180547e7882739850120c882f56f3a963a
Van Say Nguyen <vansay@hotmail.fr>
2020-02-19 12:02:13 +0100

    rails new

---
+++ app/javascript/src/app.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id='app'>
+    <p>{{ message }}</p>
+    <counter></counter>
+  </div>
+
+</template>
+
+<script>
+  
+import Counter from './components/Counter.vue'
+
+export default {
+  components: { Counter },
+  data: function () {
+    return {
+      message: "Super enfin ça marche!"
+    }
+  }
+}
+</script>
---
+++ app/javascript/src/components/Counter.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="counter">
+        <button>Un bouton</button>
+        <p class="card card-body">{{count}}</p>
+        <p class="card card-body">{{count}}</p>
+        <button class="counter_button btn btn-primary" @click="increment">Plus</button>
+        <button class="counter_button btn btn-success" @click="decrement">Moins</button>
+    </div>
+
+</template>
+
+
+
+<script>
+
+export default {
+    data () {
+        return {
+            count: 0
+        }
+    },
+
+    methods:  {
+        increment () {
+             this.count ++
+        },
+        decrement () {
+            this.count--
+        }
+       
+    }
+    
+
+
+}
+</script>
